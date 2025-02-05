94c6e7acd9058006ca325f590bc34ff3bf03c238
Your Name <your.email@example.com>
2025-02-05 15:42:57 -0500

    Task 1 - Customer Profile

---
+++ README.md
@@ -0,0 +1,3 @@
+Coding Challenge 5!
+
+In this challenge, we will practice working with JavaScript objects, array methods, and various function types in business-related scenarios.
---
+++ cc_5.js
@@ -0,0 +1,21 @@
+// Task 1: Customer Profile 
+
+let customer = { name: "John Doe", age: 35, email: "john.doe@email.com"
+    };
+
+    console.log(`Customer Information:
+        name: ${customer.name}, 
+        Age: ${customer.age}, 
+        Email: ${customer.email}`);
+
+
+
+    
+
+
+
+
+
+
+
+
